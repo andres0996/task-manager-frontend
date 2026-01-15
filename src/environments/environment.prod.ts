77437415cfba1f29969a8368931056157645fb5c
AndresAlv777 <151855872+AndresAlv777@users.noreply.github.com>
2026-01-14 19:18:19 -0500

    chore: add environment configuration files

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  apiUrl: 'https://us-central1-task-manager-app-79f86.cloudfunctions.net/api/v1'
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: false,
+  apiUrl: 'https://us-central1-task-manager-app-79f86.cloudfunctions.net/api/v1'
+};
